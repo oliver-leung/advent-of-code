38bd865c790797054a8bae576fad9ce27e5ee48c
Oliver Leung <oal22@cornell.edu>
2021-12-04 01:33:45 -0500

    Complete day3

--- 2021/day03.py
+++ 2021/day03.py
@@ -23,8 +23,43 @@ def part1(data: List[str]):
     eps = int(eps, base=2)
     return gam * eps
 
+def part2(data: List[str]):
+    oxy = data
+    co2 = data
+    size = len(data[0])
+
+    for i in range(size):
+        ones = sum(1 for n in oxy if n[i] == '1')
+        half = len(oxy) / 2
+        if ones >= half:
+            oxy = [n for n in oxy if n[i] == '1']
+        else:
+            oxy = [n for n in oxy if n[i] == '0']
+
+        if len(oxy) == 1:
+            break
+
+
+    for i in range(size):
+        zeros = sum(1 for n in co2 if n[i] == '0')
+        half = len(co2) / 2
+        if zeros <= half:
+            co2 = [n for n in co2 if n[i] == '0']
+        else:
+            co2 = [n for n in co2 if n[i] == '1']
+        
+        if len(co2) == 1:
+            break
+
+    oxy = int(oxy[0], base=2)
+    co2 = int(co2[0], base=2)
+
+    print(oxy, co2)
+    return oxy * co2
 
 if __name__ == '__main__':
     data = parse('day03.txt')
     ans1 = part1(data)
     print(ans1)
+    ans2 = part2(data)
+    print(ans2)
